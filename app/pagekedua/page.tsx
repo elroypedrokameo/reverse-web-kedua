dd11d1f4c4d84459ee97ba80a9c5783b603ca6d9
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 10:31:38 +0700

    refactor: enhance page layout and update deployment description

--- app/pagekedua/page.tsx
+++ app/pagekedua/page.tsx
@@ -1,8 +1,10 @@
 function page() {
   return (
-    <div>
-      Page Kedua di project reverse web kedua yang saya deploy di{" "}
-      <span className="text-red-500">cloudflare workers</span>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p>
+        Project kedua deploy di <span className="text-red-500">cloudflare</span>
+      </p>
+
       <a href="/" className="text-blue-500 underline">
         Balik Ke Page UTAMA <span className="text-red-500">vercel</span>
       </a>
