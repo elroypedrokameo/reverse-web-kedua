4e8e5337f613012fbf94605c1a171c67bf26f4e5
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:12:51 +0700

    first commit

---
+++ app/pagekedua/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      Page Kedua di project reverse web kedua
+      <Link href="/">Balik Ke Page UTAMA</Link>
+    </div>
+  );
+}
+
+export default page;
