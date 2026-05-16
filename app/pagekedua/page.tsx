8ed24df6f179831f569a5da3f0b574877b93a8d1
infinitygamexray <infinitygamexray@gmail.com>
2026-05-16 09:48:49 +0700

    refactor: update page content to include deployment details and enhance styling

--- app/pagekedua/page.tsx
+++ app/pagekedua/page.tsx
@@ -1,9 +1,10 @@
 function page() {
   return (
     <div>
-      Page Kedua di project reverse web kedua
+      Page Kedua di project reverse web kedua yang saya deploy di{" "}
+      <span className="text-red-500">cloudflare workers</span>
       <a href="/" className="text-blue-500 underline">
-        Balik Ke Page UTAMA
+        Balik Ke Page UTAMA <span className="text-red-500">vercel</span>
       </a>
     </div>
   );
